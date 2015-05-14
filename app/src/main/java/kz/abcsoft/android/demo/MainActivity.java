ca6c2d82842671a230bb4d277420bfd165bdb945
Daulet Baimbetov <daucloudlab@gmail.com>
2015-05-14 14:37:41 +0600

    version 004

--- app/src/main/java/kz/abcsoft/android/demo/MainActivity.java
+++ app/src/main/java/kz/abcsoft/android/demo/MainActivity.java
@@ -11,44 +11,65 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.Window;
+import android.widget.ArrayAdapter;
 
 
-public class MainActivity extends ActionBarActivity implements ActionBar.TabListener{
+public class MainActivity extends ActionBarActivity implements ActionBar.OnNavigationListener
+//implements ActionBar.TabListener
+{
 
     final String LOG_TAG = "myLogs";
+
+    String[] data = new String[] { "one", "two", "three" };
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
 
+//        ActionBar bar = getSupportActionBar() ;
+//        bar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+//
+//        ActionBar.Tab tab1 = bar.newTab() ;
+//        tab1.setText("Tab 1") ;
+//        tab1.setTabListener(this) ;
+//        bar.addTab(tab1);
+//
+//        ActionBar.Tab tab2 = bar.newTab() ;
+//        tab2.setText("Tab 2") ;
+//        tab2.setTabListener(this) ;
+//        bar.addTab(tab2);
+
         ActionBar bar = getSupportActionBar() ;
-        bar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        bar.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
 
-        ActionBar.Tab tab1 = bar.newTab() ;
-        tab1.setText("Tab 1") ;
-        tab1.setTabListener(this) ;
-        bar.addTab(tab1);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,
+                data) ;
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        bar.setListNavigationCallbacks(adapter, this);
 
-        ActionBar.Tab tab2 = bar.newTab() ;
-        tab2.setText("Tab 2") ;
-        tab2.setTabListener(this) ;
-        bar.addTab(tab2);
     }
 
     @Override
-    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
-        Log.d(LOG_TAG, "selected tab: " + tab.getText());
+    public boolean onNavigationItemSelected(int itemPosition, long itemId) {
+        Log.d(LOG_TAG, "selected: position = " + itemPosition + ", id = "
+                + itemId + ", " + data[itemPosition]);
+        return false;
     }
 
-    @Override
-    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
-        Log.d(LOG_TAG, "unselected tab: " + tab.getText());
-    }
-
-    @Override
-    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
-        Log.d(LOG_TAG, "reselected tab: " + tab.getText());
-    }
+//    @Override
+//    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+//        Log.d(LOG_TAG, "selected tab: " + tab.getText());
+//    }
+//
+//    @Override
+//    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+//        Log.d(LOG_TAG, "unselected tab: " + tab.getText());
+//    }
+//
+//    @Override
+//    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+//        Log.d(LOG_TAG, "reselected tab: " + tab.getText());
+//    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
